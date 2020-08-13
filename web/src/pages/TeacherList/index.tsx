f2d473fbf446608dd356fb0349ce767ff4133856
cicerorod <cicerorod@gmail.com>
2020-08-13 02:52:20 -0300

    CSS da tela para estudar

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,99 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
+
+import PageHeader from "../../components/PageHeader";
+import TeacherItem, { Teacher } from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
+import api from "../../services/api";
+
+import "./style.css";
 
 function TeacherList() {
-  return <div>TeacherList</div>;
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState("");
+  const [week_day, setWeek_day] = useState("");
+  const [time, setTime] = useState("");
+
+  async function searchTeachers(e: FormEvent) {
+    e.preventDefault();
+
+    const response = await api.get("classes", {
+      params: {
+        subject,
+        week_day,
+        time,
+      },
+    });
+
+    setTeachers(response.data);
+  }
+
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers" onSubmit={searchTeachers}>
+          <Select
+            name="subject"
+            label="Materia"
+            value={subject}
+            onChange={(e) => {
+              setSubject(e.target.value);
+            }}
+            options={[
+              { value: "Literatura", label: "Literatura" },
+              { value: "Discurso", label: "Discurso" },
+              {
+                value: "Escrita ou Composição",
+                label: "Escrita ou Composição",
+              },
+              { value: "Algebra", label: "Algebra" },
+              { value: "Algebra 2", label: "Geometria" },
+              { value: "História Mundial", label: "História Mundial" },
+              { value: "Espanhol", label: "Espanhol" },
+              { value: "Alemão", label: "Alemão" },
+              { value: "Françes", label: "Françes" },
+              { value: "Química", label: "Química" },
+              { value: "Física", label: "Física" },
+            ]}
+          />
+          <Select
+            name="weed_day"
+            label="Dia da Semana"
+            value={week_day}
+            onChange={(e) => {
+              setWeek_day(e.target.value);
+            }}
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda" },
+              { value: "2", label: "Terça" },
+              { value: "3", label: "Quarta" },
+              { value: "4", label: "Quinta" },
+              { value: "5", label: "Sexta" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
+          <Input
+            type="time"
+            name="time"
+            label="Hora"
+            value={time}
+            onChange={(e) => {
+              setTime(e.target.value);
+            }}
+          />
+
+          <button type="submit">Search</button>
+        </form>
+      </PageHeader>
+      <main>
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />;
+        })}
+      </main>
+    </div>
+  );
 }
+
 export default TeacherList;
